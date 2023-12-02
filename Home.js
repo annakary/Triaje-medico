57353671b527e5be2fe6c57d5dd45f2da69eb2cd
annakary <59904505+annakary@users.noreply.github.com>
2023-12-01 20:37:40 -0700

    alta de paciente completada nomas falta validar caracteres

--- Home.js
+++ Home.js
@@ -21,32 +21,32 @@ const Home = () => {
 
   return (
     <View style={tw`flex-1 items-center justify-center relative`}>
-      <View style={tw`bg-[#102536] w-full top-0 absolute z-20`}>
+      <View style={tw`bg-[#102536] w-full top-0 `}>
         <Text style={tw`text-white  text-3xl tracking-widest	pl-5  bottom-5 mt-10`}>Triaje Médico</Text>
       </View>
       <ScrollView>
         <TouchableOpacity onPress={handleEmergencyDicharge}>
-          <View style={tw`w-full bg-gray-200	top-20 px-15 py-7  items-center rounded-xl`}>
+          <View style={tw`w-full bg-gray-200	top-10 px-15 py-7  items-center rounded-xl`}>
             <Image style={tw` `} source={require('./assets/icono-urgencia.png')} />
             <Text style={tw`text-gray mt-5 text-3xl tracking-widest`}>Alta de Urgencia</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={handleDischargePatient}>
-        <View style={tw`w-full bg-gray-200 mt-10	top-20 px-15 py-7  items-center rounded-xl`}>
+        <View style={tw`w-full bg-gray-200 mt-20	 px-15 py-7  items-center rounded-xl`}>
           <Image style={tw` `} source={require('./assets/icono-alta-paciente.png')} />
           <Text style={tw`text-gray mt-5 text-3xl tracking-widest`}>Alta de Paciente</Text>
         </View>
         </TouchableOpacity>
         
         <TouchableOpacity onPress={handlePatientList}>
-        <View style={tw`w-full bg-gray-200 mt-10	top-20 px-15 py-7  items-center rounded-xl`}>
+        <View style={tw`w-full bg-gray-200 mt-10	 px-15 py-7  items-center rounded-xl`}>
           <Image style={tw` `} source={require('./assets/icono-lista-pacientes.png')} />
           <Text style={tw`text-gray mt-5 text-3xl tracking-widest`}>Lista de Pacientes</Text>
         </View>
         </TouchableOpacity>
         
         <TouchableOpacity onPress={handleInformation}>
-        <View style={tw`w-full bg-gray-200 mt-10	top-20 px-15 py-7  items-center rounded-xl`}>
+        <View style={tw`w-full bg-gray-200 mt-10  px-15 py-7  items-center rounded-xl`}>
           <Image style={tw` `} source={require('./assets/icono-info.png')} />
           <Text style={tw`text-gray mt-5 text-3xl tracking-widest`}>Información</Text>
         </View>
