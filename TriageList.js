9eb55b1cf142aa1652461e5cdb4cd0409fbc8c92
annakary <59904505+annakary@users.noreply.github.com>
2023-12-03 17:34:07 -0700

    Triage list completado solo faltan filtros, se agregaron botones y  modal

--- TriageList.js
+++ TriageList.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TextInput, ScrollView,Modal, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import { useNavigation } from '@react-navigation/native';
 
 const TriageList = () => {
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
 
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
 
   return (
     <View style={tw`flex-1 items-center justify-center relative bg-white`}>
@@ -28,8 +32,11 @@ const TriageList = () => {
         <View style={tw`w-full bg-gray-200  top-10 rounded-xl flex-row `}>
           <Image style={tw`mr-3`} source={require('./assets/triaje-1.png')} />
           <View>
-            <Text style={tw`text-black mt-5 text-3xl tracking-widest`}>Carlos Martinez</Text>
-            <Text style={tw`text-black text-lg tracking-widest mb-2`}>Accidente automovilístico</Text>
+            <Text style={tw`text-black mt-3 text-3xl tracking-widest`}>Carlos Martinez</Text>
+            <Text style={tw`text-black text-lg tracking-wider mb-2`}>Accidente automovilístico</Text>
+          <TouchableOpacity  onPress={toggleModal} style={tw`bg-gray-300 w-2/4 aling-center ml-auto  rounded-lg py-1`}>
+            <Text style={tw`text-black text-lg text-center tracking-wider mb-2`}> Ingresar</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
@@ -37,8 +44,11 @@ const TriageList = () => {
         <View style={tw`w-full bg-gray-200 mt-5 	top-10  rounded-xl flex-row`}>
           <Image style={tw`mr-3 `} source={require('./assets/triaje-2.png')} />
            <View>
-            <Text style={tw`text-black text-3xl mt-5 tracking-widest`}>Carlos Martinez</Text>
-            <Text style={tw`text-black text-lg tracking-widest mb-2`}>Accidente automovilístico</Text>
+            <Text style={tw`text-black text-3xl mt-3 tracking-widest`}>Carlos Martinez</Text>
+            <Text style={tw`text-black text-lg tracking-wider mb-2`}>Accidente automovilístico</Text>
+          <TouchableOpacity style={tw`bg-gray-300 w-2/4 aling-center ml-auto  rounded-lg py-1`}>
+            <Text style={tw`text-black text-lg text-center tracking-wider mb-2`}> Ingresar</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
@@ -47,8 +57,11 @@ const TriageList = () => {
         <View style={tw`w-full bg-gray-200 mt-5	top-10   rounded-xl flex-row`}>
           <Image style={tw`mr-3 `} source={require('./assets/triaje-3.png')} />
           <View>
-            <Text style={tw`text-black text-3xl mt-5 tracking-widest`}>Carlos Martinez</Text>
-            <Text style={tw`text-black text-lg tracking-widest mb-2`}>Accidente automovilístico</Text>
+            <Text style={tw`text-black text-3xl mt-3 tracking-widest`}>Carlos Martinez</Text>
+            <Text style={tw`text-black text-lg tracking-wider mb-2`}>Accidente automovilístico</Text>
+            <TouchableOpacity style={tw`bg-gray-300 w-2/4 aling-center ml-auto  rounded-lg py-1`}>
+            <Text style={tw`text-black text-lg text-center tracking-wider mb-2`}> Ingresar</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
@@ -57,23 +70,48 @@ const TriageList = () => {
         <View style={tw`w-full bg-gray-200 mt-5	top-10   rounded-xl flex-row`}>
           <Image style={tw`mr-3 `} source={require('./assets/triaje-4.png')} />
           <View>
-            <Text style={tw`text-black text-3xl mt-5 tracking-widest`}>Carlos Martinez</Text>
-            <Text style={tw`text-black text-lg tracking-widest mb-2`}>Accidente automovilístico</Text>
+            <Text style={tw`text-black text-3xl mt-3 tracking-widest`}>Carlos Martinez</Text>
+            <Text style={tw`text-black text-lg tracking-wider mb-2`}>Accidente automovilístico</Text>
+            <TouchableOpacity style={tw`bg-gray-300 w-2/4 aling-center ml-auto  rounded-lg py-1`}>
+            <Text style={tw`text-black text-lg text-center tracking-wider mb-2`}> Ingresar</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
         <View style={tw`w-full bg-gray-200 mt-5	top-10   rounded-xl flex-row`}>
           <Image style={tw`mr-3`} source={require('./assets/triaje-5.png')} />
           <View>
-            <Text style={tw`text-black text-3xl mt-5 tracking-widest`}>Carlos Martinez</Text>
-            <Text style={tw`text-black text-lg tracking-widest mb-2`}>Accidente automovilístico</Text>
+            <Text style={tw`text-black text-3xl mt-3 tracking-widest`}>Carlos Martinez</Text>
+            <Text style={tw`text-black text-lg tracking-wider mb-2`}>Accidente automovilístico</Text>
+            <TouchableOpacity style={tw`bg-gray-300 w-2/4 aling-center ml-auto  rounded-lg py-1`}>
+            <Text style={tw`text-black text-lg text-center tracking-wider mb-2`}> Ingresar</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
 
       </ScrollView>
 
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        {/* Contenido del modal */}
+        <View style={tw`flex-1 justify-center items-center`}>
+          <View style={tw`bg-white p-5 rounded-lg items-center`}>
+          <Image style={tw`mr-3 `} source={require('./assets/icono-modal.png')} />
 
+            <Text style={tw`text-black text-lg mt-3`}>Aún existe un paciente {'\n'} con mayor prioridad</Text>
+            <TouchableOpacity style={tw`bg-gray-300 w-2/4 m-auto  rounded-lg px-4 py-2 mt-3`} onPress={toggleModal}>
+              <Text style={tw`text-black text-center`}>Aceptar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
 
     </View>
